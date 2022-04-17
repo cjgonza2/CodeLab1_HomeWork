4c400ffbd8f65639a1109c9fe141350cbf5181f5
cjgonza2 <camrong520@gmail.com>
2022-04-17 14:59:14 -0400

    commenting

--- CodeLab1 - HW 2/Assets/HW 7/Scripts/HW7PlayerController.cs
+++ CodeLab1 - HW 2/Assets/HW 7/Scripts/HW7PlayerController.cs
@@ -11,13 +11,13 @@ public class HW7PlayerController : MonoBehaviour
 
     void Update()
     {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
-        Vector3 playerDircetion = new Vector3(horizontal, 0f, vertical).normalized;
+        float horizontal = Input.GetAxisRaw("Horizontal"); //creates a horizontal float value based on the Axis raw Value
+        float vertical = Input.GetAxisRaw("Vertical");  //creates a vertical float value based on axis raw value. 
+        Vector3 playerDircetion = new Vector3(horizontal, 0f, vertical).normalized; //creates a vector 3 for the  direction player is going and normalizes it. Normalized because we don't need to know the specific value of the direciton, we just need the direction.
 
         if (playerDircetion.magnitude >= 0.1f) //checks if the player is moving in any direction. 
         {
-            controller.Move(playerDircetion * moveSpeed * Time.deltaTime);
+            controller.Move(playerDircetion * moveSpeed * Time.deltaTime); //moves the character controller based on the vector 3 times movespeed and in relation to delta time. 
         }
     }
 
